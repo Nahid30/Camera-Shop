305cd6542a83147ca0304b7813ff0c207e3649c1
Nahid30 <nahidshuvo30@gmail.com>
2022-03-26 21:45:54 +0600

    added answers of two Questions using bootstrap accordation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Header/Shop/Shop';
+import Question from './components/Question/Question';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Header></Header>
       <Shop></Shop>
+      <Question></Question>
     </div>
   );
 }
---
+++ src/components/Question/Question.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './Question.css'
+
+const Question = () => {
+    return (
+
+        <div className='question'>
+            
+
+            <div class="accordion" id="accordionExample">
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                <h2>1.How React Works</h2>
+                </button>
+                </h2>
+                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                
+                    <p>React is a javascript's library. React make UI easily if you understand.In react you dont need to code repeatedly rather once you code you can use it from anywhere.React introduced an idea that similar in look different in data, in this idea you can create one structure for many for different datas.React have also JSX concepts that you can easily write JS HTML with also JS code in one file.So that is how React works!!</p>
+
+                </div>
+                </div>
+            </div>
+            
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingTwo">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                <h2>2.How useState Works</h2>
+                </button>
+                </h2>
+                <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    
+                    <p>useState is a Hook that allows you to have state variables in functional components.useState is using for to change something.For example you declare a function and it return a variable with the current state value and you can change or update the value in this state to another function and set the data to that initial state.Finally you can say that you need a data , array , obj, or function etc and you set a initial state and you can use,change or update whenever you need!!!</p>
+
+                </div>
+                </div>
+            </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Question;
